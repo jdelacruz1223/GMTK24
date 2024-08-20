6880109524f8a7cb835e9d2d7e1d6f2c24fe32c5
toonlank <50122169+toonlank@users.noreply.github.com>
2024-08-20 02:06:46 -0700

    added quick failsafe for dialogue

--- fried eggplant/Assets/Scripts/Managers/DialogueManager.cs
+++ fried eggplant/Assets/Scripts/Managers/DialogueManager.cs
@@ -48,6 +48,12 @@ public class DialogueManager : MonoBehaviour
         currentBox = dialogueBoxes.Dequeue();
         currentBox.SetActive(true);
     }
+    //Dequeues all active dialogue
+    public void DequeueDialogue() {
+        while(dialogueBoxes.Count != 0) {
+            dialogueBoxes.Dequeue();
+        }
+    }
     public bool isTextBoxActive() {
         return currentBox != null && currentBox.activeSelf;
     }
